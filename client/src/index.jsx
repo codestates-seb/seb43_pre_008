4080abec63f90f585ea0dc1c30aa20c857d7bcb1
Bumpist <104641096+parksubeom@users.noreply.github.com>
2023-04-17 00:25:13 +0900

    프론트엔드 초기 세팅

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import GlobalStyle from "./style/GlobalStyle";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
@@ -11,6 +12,7 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <GlobalStyle />
       <App />
     </Provider>
   </React.StrictMode>,
