597ec0575b2c3f7c66b024e3bcd6c19c5fb1240a
Bumpist <104641096+parksubeom@users.noreply.github.com>
2023-04-17 09:41:53 +0900

    feat : 페이지별 경로 설정

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,13 @@
 //import { Counter } from "./features/counter/Counter";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import Main from "./pages/Main";
 import "./App.css";
+import Main from "./pages/Main";
+import Mypage from "./pages/Mypage";
+import Ask from "./pages/Ask";
+import Notfound from "./pages/Notfound";
+import Question from "./pages/Question";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
@@ -9,6 +15,12 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<Main />} />
+          <Route path="/ask" element={<Ask />} />
+          <Route path="/mypage" element={<Mypage />} />
+          <Route path="/notfound" element={<Notfound />} />
+          <Route path="/question" element={<Question />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
       </div>
     </BrowserRouter>
