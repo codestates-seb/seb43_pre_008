ab772bd79d8f2405e7c24c5df954f17c2a763fc1
gjy0605 <gjy0605@gmail.com>
2023-04-19 22:01:07 +0900

    style: 경로 오류 수정 및 컴포넌트 설명 주석 추가

--- client/src/pages/Mypages/Mypage.jsx
+++ client/src/pages/Mypages/Mypage.jsx
@@ -5,10 +5,8 @@ import Nav from "../../share/Nav";
 import Footer from "../../share/Footer";
 import MyPage_header from "../../components/mypage/MyPage_header";
 import MyPage_menu from "../../components/mypage/MyPage_menu";
-// import Mypage_activeNav from "../../components/mypage/Mypage_activeNav";
-import MyPage_profile from "../../components/mypage/Mypage_profile";
-
-// import Layout from "../share/Layout";
+import MyPage_profile from "../../components/mypage/MyPage_profile";
+/** 2024/4/19 마이페이지 Nav와 컨텐츠(좌,우) 감싸는 Div -by 고정윤 */
 const MainDiv = styled.div`
   margin: 0 auto;
   display: flex;
@@ -16,12 +14,11 @@ const MainDiv = styled.div`
   width: 100%;
   max-width: 1264px;
 `;
-
+/** 2024/4/19 마이페이지 컨텐츠만 감싸는 Div -by 고정윤 */
 const Container = styled.div`
-  /* margin: 0px 65px 0px 65px; */
   width: 100%;
 `;
-
+/** 2024/4/19 마이페이지 Content -by 고정윤 */
 const Content = styled.div`
   display: flex;
   flex-direction: column;
@@ -29,14 +26,15 @@ const Content = styled.div`
   font-size: 0.8rem;
 `;
 
+/** 2024/4/19 마이페이지 MainContent -by 고정윤 */
 const MainContent = styled.div`
   display: flex;
 `;
-
+/** 2024/4/19 마이페이지 MainContent -by 고정윤 */
 const Section = styled.div`
   width: 100%;
 `;
-
+/** 2024/4/19 Mypage 컴포넌트 -by 고정윤 */
 export default function Mypage() {
   // const [isLoading, setIsLoding] = useState(true);
 
@@ -53,7 +51,6 @@ export default function Mypage() {
             <MyPage_header />
             <MyPage_menu />
             <MainContent>
-              {/* <Mypage_activeNav /> */}
               <Section>
                 <MyPage_profile />
               </Section>
