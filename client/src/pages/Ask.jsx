de583644c010a2e41ba0a167799e70cf3737e863
gjy0605 <gjy0605@gmail.com>
2023-04-21 00:54:06 +0900

    design: 질문하기 페이지

--- client/src/pages/Ask.jsx
+++ client/src/pages/Ask.jsx
@@ -1,10 +1,26 @@
-import Layout from "../share/Layout";
+import React from "react";
+import styled from "styled-components";
+import Header from "../share/Header";
+import Footer from "../share/Footer";
 
-export default function Ask() {
+const Container = styled.div`
+  width: 100%;
+  padding: 0 24px;
+  display: flex;
+  justify-content: center;
+  height: 200px;
+  background-color: red;
+`;
+const Content = styled(Container)``;
+const Ask = () => {
   return (
-    <div>
-      <Layout />
-      질문작성 페이지입니다
-    </div>
+    <React.Fragment>
+      <Header />
+      <Container>
+        <Content></Content>
+      </Container>
+      <Footer />
+    </React.Fragment>
   );
-}
+};
+export default Ask;
