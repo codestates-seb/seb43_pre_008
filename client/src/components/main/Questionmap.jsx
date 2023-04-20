210310749c4967fa266c8587be2e9b3b4c0cd724
Bumpist <104641096+parksubeom@users.noreply.github.com>
2023-04-20 09:57:20 +0900

    style : jsdocs 설명 추가

--- client/src/components/main/Questionmap.jsx
+++ client/src/components/main/Questionmap.jsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import Questionlist from "./Questionlist";
-
+/** 2023/04/20 - 질문 리스트 감싸는 컴포넌트 - by 박수범 */
 const QuestionListul = styled.ul`
   display: flex;
   flex-direction: column;
